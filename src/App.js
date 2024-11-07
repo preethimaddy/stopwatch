19ab845a451aed8d2f690a10d07e7b8cba7b5050
Preethi Madhavan <98338783+preethimaddy@users.noreply.github.com>
2024-11-07 16:27:48 +0530

    StopWatch start stop rest operation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState,useEffect } from 'react';
 function App() {
+
+  const[time,setTime] = useState(0);
+  const [running, setRunning] = useState(false)
+
+  useEffect(()=>{
+   let interval;
+    if(running){
+       interval=setInterval(()=>{
+        setTime((prevTime)=> prevTime + 10)
+      }, 10)
+    } 
+    else if(!running){
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  },[running]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <h1>Stop Watch</h1>
+   <div>
+   <span>{("0" + Math.floor((time/60000) % 60)).slice(-2)}:</span>
+   <span>{("0" + Math.floor((time/1000) % 60)).slice(-2)}:</span>
+   <span>{("0" + (time/10) % 100).slice(-2)}:</span>
+   </div>
+   <div>
+    <button onClick={() =>setRunning(true)}>start</button>
+    <button onClick={() =>setRunning(false)}>stop</button>
+    <button onClick={() =>setTime(0)}>Reset</button>
+   </div>
+  </>
+
   );
 }
 
