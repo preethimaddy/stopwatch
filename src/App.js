978836666ac98631e15ffaea1aeab34546d1de59
Preethi Madhavan <98338783+preethimaddy@users.noreply.github.com>
2024-11-07 17:35:41 +0530

    start stop tailwind css

--- src/App.js
+++ src/App.js
@@ -20,19 +20,32 @@ function App() {
 
   return (
   <>
-    <h1>Stop Watch</h1>
+  <div className='flex flex-col items-center justify-center py-8'>
+    <h1 className='text-2xl font-semibold'>Stop Watch</h1>
    <div>
    <span>{("0" + Math.floor((time/60000) % 60)).slice(-2)}:</span>
    <span>{("0" + Math.floor((time/1000) % 60)).slice(-2)}:</span>
    <span>{("0" + (time/10) % 100).slice(-2)}:</span>
    </div>
-   <div>
-    {running ? (  <button onClick={() =>setRunning(false)}>Stop</button>
+   <div className='w-1/3 max-w-sm flex flex-row justify-between'>
+    {running ? (  
+      <button 
+      className='border rounded-lg py-1 px-3.5'
+    onClick={() =>setRunning(false)}>
+      Stop
+      </button>
        ):(
-  <button onClick={() =>setRunning(true)}>Start</button>)}
+  <button 
+  className='bg-green-500 hover:bg-green-700 focus:bg-green-600 active:bg-green-800 border rounded-lg py-1 px-3'
+  onClick={() =>setRunning(true)}>
+    Start
+    </button>)}
 
  
-    <button onClick={() =>setTime(0)}>Reset</button>
+    <button 
+    className='bg-red-500 hover:bg-red-700 focus:bg-red-600 active:bg-red-800 border rounded-lg py-1 px-2.5' 
+    onClick={() =>setTime(0)}>Reset</button>
+   </div>
    </div>
   </>
 
